26aad5f38e5a356abf3a39730a49e6834af352f8
Sezin Kokum Mutludoğan <sezinkokum@gmail.com>
2023-04-03 04:08:13 +0300

    typescript - ADD/DELETE items

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,82 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Input from './components/input';
+import Message from './components/message';
+import { Todos } from './types/Type';
 
-function App() {
+const App:React.FC = () => {
+ // let name: string = "Sezin";
+ // let age: number = 30;
+ // let done: boolean = true;
+ // let trial: number | string = 5 
+
+ // let trial: any = "deneme"
+ // let trial2: number | string = "deneme" ıkı turlu de atansa hata almaz
+ // let trial3: number | string = 5
+
+  /*type Obj = {
+    name: string,
+    age: number,
+    done?: boolean
+  }
+
+  type ObjTwo extends Obj= {
+    //name: string, 
+    //age: number,
+    //done?: boolean,
+    accept?: boolean
+  }*/
+
+  /*interface Obj {
+    name: string,
+    age: number,
+    done?: boolean
+  }
+
+  interface ObjTwo extends Obj{
+    accept?: boolean
+  }
+
+  let obj: Obj = {
+    name: "Sezin",
+    age: 30,
+    //done: true
+  }*/
+
+  /*return (
+    <div className="App">
+      {name + " - " + age}
+    </div>
+  );*/
+
+  const [todo, setTodo] = useState<string>('')
+  const [todos, setTodos] = useState<Todos[]>([])
+
+/*  Todos yapısı bunlardan birsürü de olabilir
+[  
+  {
+  message:"sezin",
+  id:1
+  }
+]*/
+
+    console.log(todos,"todos")
+
+    const addMessage = () => {
+      if(todo){
+        setTodos([...todos,{message: todo,id: todos.length+1}])
+        setTodo('')
+      }
+    }
+
+    const deleteMessage = (id: number) => {
+      setTodos(todos.filter(todo => todo.id != id))
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Input addMessage={addMessage} todo ={todo} setTodo = {setTodo}/>    
+        <Message deleteMessage ={deleteMessage} todos ={todos}/>
     </div>
   );
 }
